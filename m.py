73b62bfd7a67a72b0a6a505db4b25e6d25b7890d
Mood <sreee-@hotmail.com>
2022-06-13 19:21:49 +0100

    some tips

--- m.py
+++ m.py
@@ -1,9 +1,2 @@
-print("mood")
-print("mo55od")
-print("mo5555od")
-print("mo5555offd66565")
-print("mojsd55")
-print("moj55")
-print("mo5j5")
-print("modejddgfpdjgjrg55")
-print("mo55")
+print('''Hello guys
+i'm here''')
